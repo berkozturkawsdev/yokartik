9c2dd7980fb59680af76c1f90beadc359e3da5a2
berkozturkawsdev@gmail.com <berkozturkawsdev@gmail.com>
2026-08-07 14:26:34 +0000

    add some news

--- src/data/news.ts
+++ src/data/news.ts
@@ -3,12 +3,21 @@ import type { News } from "../types/News";
 import rawKatyPerryBottle from "./news/katy-perry-bottle.json";
 import rawAnimeFanOrder from "./news/anime-fan-order.json";
 import rawDdgSunglasses from "./news/ddg-sunglasses.json";
+import rawAsmrBan from "./news/asmr-ban.json";
+import rawNewOrleans911Ai from "./news/new-orleans-911-ai.json";
+import rawGoogleFlockGold from "./news/google-flock-gold.json";
 
 const katyPerryBottle: News = rawKatyPerryBottle as unknown as News;
 const animeFanOrder: News = rawAnimeFanOrder as unknown as News;
 const ddgSunglasses: News = rawDdgSunglasses as unknown as News;
+const asmrBan: News = rawAsmrBan as unknown as News;
+const newOrleans911Ai: News = rawNewOrleans911Ai as unknown as News;
+const googleFlockGold: News = rawGoogleFlockGold as unknown as News;
 
 export const news: News[] = [
+    googleFlockGold,
+    newOrleans911Ai,
+    asmrBan,
     ddgSunglasses,
     animeFanOrder,
     katyPerryBottle,
